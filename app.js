b89ce8f575c94fbdc65ff8f93146dcb83f65c02e
mevans72 <michaleevans72@gmail.com>
2016-02-15 09:15:31 -0800

    Knocking out problem two!

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 function sum() {
   var ansProbOne = (4 + 7);
   console.log('The sum of 4 and 7 is: '+ ansProbOne);
-}
+};
+
+function multiply() {
+  var ansProbTwo = (4 * 7);
+  console.log('The product of 4 and 7 is: ' + ansProbTwo);
+};
